5fa83f7d53139c27423623b43897b0519ce27f99
emad <arabanemad@gmail.com>
2025-12-30 11:32:44 +0330

    refactor:"Sidebar overflow and direction adjustmen

--- src/app/(dashboard-admin)/dashboardAdmin/layout.js
+++ src/app/(dashboard-admin)/dashboardAdmin/layout.js
@@ -4,33 +4,46 @@ import DashboardHeader from "@/shared/components/dashboardLayout/DashboardHeader
 import { useState } from "react";
 import { menuItemsList } from "@/features/dashboard-admin/constants";
 
-export default function AdminDashboardLayout({ children }) {
+export default function Dashboardlayout({ children }) {
   const [sideBarStatus, setSideBarStatus] = useState(false);
 
   return (
-    <div className="flex h-screen w-full bg-gray-100 overflow-hidden">
-      {/* Sidebar */}
-      <Sidebar
-        is_menu_open={sideBarStatus}
-        onToggleSidebar={() => setSideBarStatus(!sideBarStatus)}
-        menu_items_list={menuItemsList}
-      />
-
-      {/* Main Content */}
-      <div
-        className={`flex-1 flex flex-col relative ${
-          sideBarStatus ? "lg:mr-64" : ""
-        } transition-all duration-300`}
-      >
-        <DashboardHeader
-          is_side_bar_open={sideBarStatus}
-          onToggleSidebar={() => setSideBarStatus(!sideBarStatus)}
+    <div className="flex h-screen w-full bg-gray-100 ">
+      {sideBarStatus && (
+        <div
+          className="fixed inset-0 bg-black/40 z-40 lg:hidden"
+          onClick={() => setSideBarStatus(false)}
         />
-        <main
-          className={`shadow-2xl fixed left-0 flex-1 overflow-y-auto mx-0 lg:mx-5 mt-24 p-6 lg:p-8 bg-white w-full lg:w-[81%] ${
-            sideBarStatus ? "z-40" : "z-80"
-          } transition-all duration-300`}
-        >
+      )}
+
+      {/*  Sidebar */}
+      <aside
+        className={`
+          
+    fixed lg:static top-0 right-0
+    h-full w-64 bg-white z-50
+    transform transition-transform duration-300
+    lg:translate-x-0
+    ${sideBarStatus ? "translate-x-0" : "translate-x-full"}
+  `}
+      >
+        <Sidebar
+          is_menu_open={sideBarStatus}
+          onToggleSidebar={() => setSideBarStatus(!sideBarStatus)}
+          menu_items_list={menuItemsList}
+        />
+      </aside>
+
+      <div className="flex-1  flex flex-col">
+        {/* Header */}
+        <div className="sticky top-0 z-30">
+          <DashboardHeader
+            is_side_bar_open={sideBarStatus}
+            onToggleSidebar={() => setSideBarStatus(!sideBarStatus)}
+          />
+        </div>
+
+        <main className="flex-1  overflow-y-auto px-4 py-6 mt-24 ">
           {children}
         </main>
       </div>
